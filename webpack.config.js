9b8da7afd2f376ffe78c73760c9793305352c717
Jonas Schmidt <jonfelben@googlemail.com>
2021-07-19 12:01:08 +0200

    changed title

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Development',
+            title: 'Die Themen der Pandemie',
         })
     ],
     output: {
