57cde8b4ee89dc94295f65d407384c0187ead356
Jonas Schmidt <jonfelben@googlemail.com>
2021-07-19 15:03:38 +0200

    comma fix

--- src/components/results/results.js
+++ src/components/results/results.js
@@ -9,7 +9,7 @@ export const createResults = () => {
 
     const resultsIntro = document.createElement("p");
     resultsIntro.classList.add("results_intro");
-    resultsIntro.innerHTML = "Im Folgenden stellen wir die Ergebnisse unserer Untersuchungen vor. Dazu wird in Diagrammen das Auftreten der auf verschiedene Arten und Weisen definierten Themen verglichen. Wie bereits angedeutet wurde auf Basis der Pearson-Koeffizients verglichen, wie ähnlich die errechneten Werte für die Themen aus Wortfeldanalyse und topic modeling sind. Themenbereiche die sich auf dieser Grundlage ähneln, werden gemeinsam in einem Diagramm dargestellt, um einen direkten Vergleich zu bieten. Themen, die nicht über einen solchen Vergleichspartner verfügen werden einzeln dargestellt. Im Anschluss an diese Präsentation finden sich dann auch noch einmal alle Themen und ihre Verläufe als Einzeldarstellung."
+    resultsIntro.innerHTML = "Im Folgenden stellen wir die Ergebnisse unserer Untersuchungen vor. Dazu wird in Diagrammen das Auftreten der auf verschiedene Arten und Weisen definierten Themen verglichen. Wie bereits angedeutet wurde auf Basis des Pearson-Koeffizients verglichen, wie ähnlich die errechneten Werte für die Themen aus Wortfeldanalyse und topic modeling sind. Themenbereiche, die sich auf dieser Grundlage ähneln, werden gemeinsam in einem Diagramm dargestellt, um einen direkten Vergleich zu bieten. Themen, die nicht über einen solchen Vergleichspartner verfügen, werden einzeln dargestellt. Im Anschluss an diese Präsentation finden sich dann auch noch einmal alle Themen und ihre Verläufe als Einzeldarstellung."
 
     results.appendChild(resultsHeadline)
     results.appendChild(resultsIntro)
